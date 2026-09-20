5cf72d51495aad7773031e41e6d20556fa0559f0
fajri62 <reza76671@gmail.com>
2026-09-20 23:51:15 +0700

    ...

--- module-3-tipe-data/index.js
+++ module-3-tipe-data/index.js
@@ -1,3 +1,4 @@
+/*TIPE DATA PRIMITIF*/
 // string
 const nama = 'Dimas';
 const domisili = `${nama} Jakarta`;
@@ -36,13 +37,22 @@ const person = {
     umur: 20,
     isMarried: false
 };
-console.log(person, typeof person);
+console.log(person.nama);
 
 // array
-const arr = ['Dimas', 20, false];
-console.log(arr, typeof arr);
+const idd = ['Dimas', 20, false];
+console.log(idd[2]);
+
 
 // function
-function sayHello() {
-    console.log('Hello');
+// 1. Membuat fungsi
+function LuasSegitiga (alas, tinggi) {
+  return alas * tinggi / 2;
 }
+
+// 2. Memanggil fungsi
+let segitiga1 = LuasSegitiga(10, 9);
+let segitiga2 = LuasSegitiga(3, 3);
+let segitiga3 = LuasSegitiga(5,10);
+
+console.log(segitiga1, segitiga2, segitiga3);
